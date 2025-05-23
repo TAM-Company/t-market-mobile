ba3de16d0a08d8704cccff5fd4d4d3654b51887a
moasko.dev <42101671+moasko@users.noreply.github.com>
2025-05-23 22:03:26 +0000

    Update ProductCard.tsx

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -30,7 +30,7 @@ export const ProductCard: React.FC<ProductCardProps> = ({
         <Text style={styles.name} numberOfLines={1}>
           {product.name}
         </Text>
-        <Text style={styles.price}>{product.price.toFixed(2)} FCFA</Text>
+        <Text style={styles.price}>{product.price} FCFA</Text>
         <View style={styles.stockContainer}>
           <Text
             style={[
